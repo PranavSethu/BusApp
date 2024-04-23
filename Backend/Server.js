a59f5a1a788ca78f9ed2f0cf426c88b9f950e483
PranavSethu <pranavsethu5756@gmail.com>
2024-04-23 14:14:57 +0530

    changing 3

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'https://bus-app-two.vercel.app',
+    origin: 'https://bus-booking-lemon.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
